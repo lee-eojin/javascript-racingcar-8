da1676f52c74b0f3dd1631da338bef3a8e40c28d
lee-eojin <lejin0906@snu.ac.kr>
2025-10-27 16:16:07 +0900

    refactor(constants): add Object.freeze

--- src/constants.js
+++ src/constants.js
@@ -3,11 +3,11 @@ export const RANDOM_MAX_NUMBER = 9;
 export const MIN_FORWARD_VALUE = 4;
 export const MAX_NAME_LENGTH = 5;
 
-export const MESSAGES = {
+export const MESSAGES = Object.freeze({
   INPUT_NAMES: '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
   INPUT_ROUND_COUNT: '시도할 횟수는 몇 회인가요?\n',
   RESULT_MESSAGE: '\n실행 결과',
   ERROR_NAME_EMPTY: '[ERROR] 자동차 이름은 빈 값일 수 없습니다.',
   ERROR_NAME_LENGTH: '[ERROR] 자동차 이름은 5자 이하만 가능합니다.',
   ERROR_ROUND_COUNT: '[ERROR] 시도 횟수는 1 이상의 정수여야 합니다.',
-};
+});
