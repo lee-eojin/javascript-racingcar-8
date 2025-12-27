ee5856ca3bb6ff89e8afdb58ee5fa4d6b5ee764d
lee-eojin <lejin0906@snu.ac.kr>
2025-12-27 17:09:38 +0900

    feat: 5시간 안에 자동차 경주 게임 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import RaceController from './controllers/RaceController.js';
-import ConsoleInputReader from './console/ConsoleInputReader.js';
-import ConsoleOutputWriter from './console/ConsoleOutputWriter.js';
+import { Console } from "@woowacourse/mission-utils";
+import Car from "./Car";
 
 class App {
   async run() {
-    const controller = new RaceController();
-    const inputReader = new ConsoleInputReader();
-    const outputWriter = new ConsoleOutputWriter();
+    const input = await Console.readLineAsync(
+      "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"
+    );
+    const playerNames = input.split(",");
 
-    const names = await inputReader.readNames();
-    const roundCount = await inputReader.readRoundCount();
+    for (const name of playerNames) {
+      if (name.length > 5) {
+        throw new Error("[ERROR] 이름은 5자 이하여야 합니다.");
+      }
+    }
 
-    controller.initCars(names);
-    outputWriter.printResultMessage();
+    const countInput = await Console.readLineAsync(
+      "시도할 횟수는 몇 회인가요?"
+    );
+    const countNumber = Number(countInput);
+    if (Number.isNaN(countNumber) || countNumber < 1) {
+      throw new Error("[ERROR] 유효한 횟수를 입력해주세요.");
+    }
+    const cars = playerNames.map((name) => new Car(name));
+  
+    Console.print("\n실행 결과");
 
-    controller.playGame(roundCount, (cars) => {
-      outputWriter.printRoundResult(cars);
-    });
+    for (let i = 0; i < countNumber; i++) {
+      cars.forEach(car => car.tryMove());
+      cars.forEach(car => Console.print(car.getResult()));
+      Console.print("");
+    }
+    
+    const maxPosition = Math.max(...cars.map(car => car.getPosition()));
 
-    outputWriter.printWinners(controller.getWinners());
+    const winners = cars
+    .filter(car => car.getPosition() === maxPosition)
+    .map(car => car.getName());
+
+    Console.print(`최종 우승자 : ${winners.join(", ")}`);
   }
 }
 
